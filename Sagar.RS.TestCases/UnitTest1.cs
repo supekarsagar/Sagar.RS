e2563bb3e8fc1a2c96a815174d6ba0c78f40fc00
Sagar Supekar <sgrspkr@outlook.com>
2024-03-08 23:21:19 +0530

    First Commit: Folder Structure of Project

---
+++ Sagar.RS.TestCases/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sagar.RS.TestCases
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
